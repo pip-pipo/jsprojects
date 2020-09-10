d45a47fc89bdea5a6e4cbda981040d9eb31e997f
Morsalin-pip <69422979+Morsalin-pip@users.noreply.github.com>
2020-09-10 03:39:17 -1100

    hello

---
+++ js/Name_f.js
@@ -0,0 +1,41 @@
+
+//////////////////////////////////////////////////////////////////////
+// let array = {
+//     name : "mosalin",
+//     location :{
+//         x:1,
+//         y:2,
+//     },
+//         draw : function () {
+//         console
+//         },
+// };
+// array.dra();
+// function creatrad(name){
+//     return  {
+//         name,
+//         draw : function(){
+//             console.log("draw");
+//         }
+//     };
+// }
+// var d = creatrad();
+// d.draw();
+///constractor function
+// function Name_f(name) {
+//     this.name = name;
+//     this.raw = function () {
+//         console.log("draw");
+//     };
+// }
+
+
+//  const colors =[
+//         "red","blue","yeallow","green",
+//      "#eb4034","#489c76","#826dc7",
+//      "#b366ed","#e3409c","#40e39c", ];
+
+
+
+//  var styl = document.querySelector("body")[0].style.color="red";
+
---
+++ js/app.js
@@ -0,0 +1,63 @@
+// function Name_f(name) {
+//     this.name = name;
+
+//     this.raw = function () {
+//         console.log("draw");
+//     };
+// }
+// const anouther = new Name_f();
+
+
+
+// function Hello( name , brand, price){
+//     this.name = name;
+//     this.brand = brand;
+//     this.price  = price;
+//     this.my = document.querySelector(".color");
+//     console.log(my)
+// }
+
+// var x = new Hello();
+// x.my();
+
+
+
+
+
+
+
+// function me(){
+//     let sum = 0;
+//     for(var i = 0; i<arguments.length; i++){
+//         sum+=arguments(i)
+//     }
+// }
+// const x = me.apply(null , 10 ,20);
+// console.log(x);
+
+
+
+
+
+// anouther.raw();
+
+// for (let key in anouther){
+//     console.log(key,anouther[key]);
+//     if(typeof anouther[key] !== "function"){
+//         console.log(key, anouther[key]);
+//     }
+// }
+
+
+
+
+// const x = 34;
+
+// function inc(x){
+//     x.value++;
+
+// }
+// inc(x);
+// console.log(x);
+
+
